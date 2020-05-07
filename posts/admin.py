4ad490ad77910f693def35d94412588e7bc6dd24
Navneettyagi <navneet.tyagi@classicinformatics.com>
2020-05-07 12:08:20 +0530

    PostModel Class added.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Post
+        
+    
+
+admin.site.register(Post, PostModelAdmin)
